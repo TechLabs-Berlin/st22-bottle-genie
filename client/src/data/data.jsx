b4e141a4594b63f70b8e0eb838f808c653fb074e
vinntt <vinh.ngtt@gmail.com>
2022-07-04 22:04:53 +0200

    created data to test the map feature

---
+++ client/src/data/data.jsx
@@ -0,0 +1,19 @@
+export const berlinDistrictsXY = [
+    {
+      district: 'Mitte',
+      // LatLng array that represents a geographical point (the district):
+      latLng: [52.521992, 13.413244],
+    },
+    {
+        district: 'Kreuzberg',
+      latLng: [52.498604, 13.391799],
+    },
+    {
+        district: 'Friedrichshain',
+      latLng: [52.515816, 13.454293],
+    },
+    {
+        district: 'Spandau',
+      latLng: [52.532271, 13.143367],
+    }
+  ]
