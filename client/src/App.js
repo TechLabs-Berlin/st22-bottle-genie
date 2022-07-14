8a9c86864a66de6eeb0e5c15c5a45eaa91a4bef8
vinntt <vinh.ngtt@gmail.com>
2022-07-14 23:32:49 +0200

    corrected the page names

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,27 @@ import LogIn from './pages/LogIn/LogIn';
 import Pass from './components/Pass';
 import Search from './pages/Search/Search';
 import Wiki from './pages/Wiki/Wiki';
-import Homepage from './pages/Homepage/Homepage';
-import Mappage from './pages/Map/Mappage';
-import Scanpage from './pages/Scan/Scanpage';
+import HomePage from './pages/HomePage/HomePage';
+import MapPage from './pages/Map/MapPage';
+import ScanPage from './pages/Scan/ScanPage';
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
-				{/* <Route path='/' element={<Homepage />} /> */}
+				{/* <Route path='/' element={<HomePage />} /> */}
 				<Route path="/signup" element={<Signup />} />
 				<Route path="/login" element={<LogIn />} />
 				<Route path="/pass" element={<Pass />} />
 				<Route path="/search" element={<Search />} />
 				<Route path="/wiki" element={<Wiki />} />
-				<Route path="/" element={<Homepage />} />
-				<Route path="/map" element={<Mappage />} />
-				<Route path="/scan" element={<Scanpage />} />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/map" element={<MapPage />} />
+				<Route path="/scan" element={<ScanPage />} />
 				{/* <Route path="/search" element={<ProtectedRoute redirectTo='/login'><Search /></ProtectedRoute>} />
                 <Route path="/wiki" element={<ProtectedRoute redirectTo='/login'><Wiki /></ProtectedRoute>} />
-                <Route path="/" element={<ProtectedRoute redirectTo='/login'><Homepage /></ProtectedRoute>} />
-                <Route path="/map" element={<ProtectedRoute redirectTo='/login'><Mappage /></ProtectedRoute>} /> */}
+                <Route path="/" element={<ProtectedRoute redirectTo='/login'><HomePage /></ProtectedRoute>} />
+                <Route path="/map" element={<ProtectedRoute redirectTo='/login'><MapPage /></ProtectedRoute>} /> */}
 			</Routes>
 		</div>
 	);
