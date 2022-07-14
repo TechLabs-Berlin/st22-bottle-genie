5fe42b733c78847cdd0a9e3ec7f025e217c35c17
vinntt <vinh.ngtt@gmail.com>
2022-07-14 22:10:21 +0200

    edited scan function

--- client/src/components/Scan.jsx
+++ client/src/components/Scan.jsx
@@ -1,73 +1,51 @@
-import React from "react";
+// https://javascript.plainenglish.io/capture-images-via-webcam-using-react-9282bb87de5a
+import React, { Component, useState } from 'react';
+import CenterFocusWeakIcon from '@mui/icons-material/CenterFocusWeak';
 import Webcam from "react-webcam";
 
 function Scan() {
+    const [scan, setScan] = useState(null);
+    // const [image, setImage] = useState('');
+    const webcamRef = React.useRef(null);
 
+    // Video constraints define the properties of the video recording that goes on before the image is captured
     const videoConstraints = {
         width: 220,
         height: 200,
         facingMode: "user"
     };
-    const webcamRef = React.useRef(null);
 
-    return(
-        <div>
-            <Webcam
-                audio={false}
-                // height={200}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                // width={220}
-                videoConstraints={videoConstraints}
-            />
+
+    const webcam = <Webcam
+        audio={false}
+        ref={webcamRef}
+        // ScreenshotFormat returns a base64 encoded string of the current webcam image
+        screenshotFormat="image/jpeg"
+        width={300} height={300}
+        videoConstraints={videoConstraints}
+    />
+
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+    }, [webcamRef]);
+
+    const handleOnClick = (e) => {
+        e.preventDefault();
+        setScan(webcam);
+
+    }
+
+    return (
+        <div className="webcam-container">
+            <div>
+                {!scan && <button
+                    onClick={handleOnClick}>
+                    <CenterFocusWeakIcon sx={{ fontSize: "300px" }} />
+                </button>}
+                {scan}
+            </div>
         </div>
     )
 }
 
 export default Scan;
-
-// import React, { Component, useState } from 'react';
-// // import './cameraStyles.css'
-// import Webcam from "react-webcam";
-
-// const WebcamComponent = () => <Webcam />;
-// const videoConstraints = {
-//     width: 220,
-//     height: 200,
-//     facingMode: "user"
-// };
-
-// function WebcamCapture () {
-//     const webcamRef = React.useRef(null);
-
-//     const capture = React.useCallback(
-//         () => {
-//             const imageSrc = webcamRef.current.getScreenshot();
-//         },
-//         [webcamRef]
-//     );
-
-//     const handleOnClick = (e) => {
-//         e.preventDefault();
-//         capture();
-//     }
-
-//     return (
-//         <div className="webcam-container">
-//             <Webcam
-//                 audio={false}
-//                 height={200}
-//                 ref={webcamRef}
-//                 screenshotFormat="image/jpeg"
-//                 width={220}
-//                 videoConstraints={videoConstraints}
-//             />
-//             <button
-//                 onClick={handleOnClick}>
-//                 Capture
-//             </button>
-//         </div>
-//     );
-// };
-
-// export default WebcamCapture;
