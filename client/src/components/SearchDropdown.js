847786ab7622a543eb2b9d88d57c90646c105fc3
smokespirit <ania.szudek@gmail.com>
2022-07-20 18:06:35 +0200

    Search form populated w/ JSON file

--- client/src/components/SearchDropdown.js
+++ client/src/components/SearchDropdown.js
@@ -6,36 +6,46 @@ import Container from 'react-bootstrap/Container';
 import Dropdown from 'react-bootstrap/Dropdown';
 import './../asset/logo.png';
 import NavigationBar from './NavigationBar/NavigationBar';
+import SearchDataSet from '../data/searchDataSet.json';
 
 function SearchDropdown() {
+	const removeDuplicates = (array) => {
+		const set = new Set(array);
+		const iterable = set.values();
+		return Array.from(iterable);
+	};
+
+	const materialsArray = removeDuplicates(SearchDataSet.map((item) => item.Material));
+	const volumeArray = removeDuplicates(SearchDataSet.map((item) => item.Volume));
+	const brandArray = removeDuplicates(SearchDataSet.map((item) => item.Brand));
+
 	return (
 		<div className="container-fluid" id="main-container">
 			<img src={require('./../asset/logo.png')} className="img-fluid" alt="Bottle Genie app logo" id="logo" />
 			<h3 id="heading">SEARCH</h3>
-			<select className="form-select" aria-label="Default select example">
-				<option selected id="material">
-					Choose material
-				</option>
-				<option value="1">One</option>
-				<option value="2">Two</option>
-				<option value="3">Three</option>
-			</select>
-			<select className="form-select" aria-label="Default select example">
-				<option selected id="volume">
-					Choose volume
-				</option>
-				<option value="1">One</option>
-				<option value="2">Two</option>
-				<option value="3">Three</option>
-			</select>
-			<select className="form-select" aria-label="Default select example">
-				<option selected id="brand">
-					Choose brand
-				</option>
-				<option value="1">One</option>
-				<option value="2">Two</option>
-				<option value="3">Three</option>
-			</select>
+			<form>
+				<div class="form-group">
+					<select className="form-select">
+						{materialsArray.map((item) => {
+							return <option selected>Material: {item}</option>;
+						})}
+					</select>
+					<select className="form-select">
+						{volumeArray.map((item) => {
+							return <option selected>Volume: {item}</option>;
+						})}
+					</select>
+					<select className="form-select">
+						{brandArray.map((item) => {
+							return (
+								<option value="1" selected id="brand">
+									Brand: {item}
+								</option>
+							);
+						})}
+					</select>
+				</div>
+			</form>
 			<button type="button" className="btn btn-success btn-md" id="button-search">
 				Find return point
 			</button>
